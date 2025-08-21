a7e2e5bb602ad2deb516ca25bca6e22fe67df67d
Junkai Ji <98195504+swampholyten@users.noreply.github.com>
2025-08-21 11:20:17 +0200

    add treesitter

--- init.lua
+++ init.lua
@@ -25,6 +25,7 @@ vim.pack.add({
 	{ src = "https://github.com/stevearc/oil.nvim" },
 	{ src = "https://github.com/mason-org/mason.nvim" },
 	{ src = "https://github.com/saghen/blink.cmp",              version = "1.*" },
+	{ src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "main" },
 })
 
 require("mini.surround").setup()
