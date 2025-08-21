56cf439553de018938cb7cbb85923dedaf68102a
Junkai Ji <98195504+swampholyten@users.noreply.github.com>
2025-08-21 17:06:02 +0200

    make life harder

--- init.lua
+++ init.lua
@@ -1,7 +1,6 @@
 vim.g.mapleader = " "
 
 vim.opt.shiftwidth = 2
-vim.opt.hlsearch = false
 vim.opt.tabstop = 2
 vim.opt.smartindent = true
 vim.opt.wrap = false
@@ -21,17 +20,14 @@ vim.pack.add({
 	{ src = "https://github.com/ibhagwan/fzf-lua" },
 	{ src = "https://github.com/kdheepak/lazygit.nvim" },
 	{ src = "https://github.com/echasnovski/mini.surround" },
-	{ src = "https://github.com/echasnovski/mini.pairs" },
 	{ src = "https://github.com/christoomey/vim-tmux-navigator" },
 	{ src = "https://github.com/karb94/neoscroll.nvim" },
 	{ src = "https://github.com/stevearc/oil.nvim" },
 	{ src = "https://github.com/mason-org/mason.nvim" },
 	{ src = "https://github.com/saghen/blink.cmp",                version = "1.*" },
-	{ src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "main" },
 })
 
 require("mini.surround").setup()
-require("mini.pairs").setup()
 require("neoscroll").setup({ duration_multiplier = 0.5 })
 require("mason").setup()
 require("fzf-lua").setup()
@@ -56,11 +52,6 @@ vim.lsp.enable({ "rust_analyzer", "lua_ls", "vtsls", "tailwindcss", "html", "cla
 local fzf = require("fzf-lua")
 
 vim.keymap.set("n", "<leader>wo", ":wincmd c<CR>")
-vim.keymap.set("n", "<leader>ww", ":wincmd w<CR>")
-vim.keymap.set("n", "<leader>wh", ":wincmd H<CR>")
-vim.keymap.set("n", "<leader>wl", ":wincmd L<CR>")
-vim.keymap.set("n", "<leader>wi", ":wincmd I<CR>")
-vim.keymap.set("n", "<leader>wk", ":wincmd K<CR>")
 
 vim.keymap.set({ "n", "v" }, "<leader>y", '"+y')
 vim.keymap.set({ "n", "v" }, "<leader>p", '"+p')
