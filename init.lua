c4ab68d3d42432c933f3ac0c117b03bbc728325c
Junkai Ji <98195504+swampholyten@users.noreply.github.com>
2025-08-21 11:21:01 +0200

    format

--- init.lua
+++ init.lua
@@ -24,7 +24,7 @@ vim.pack.add({
 	{ src = "https://github.com/karb94/neoscroll.nvim" },
 	{ src = "https://github.com/stevearc/oil.nvim" },
 	{ src = "https://github.com/mason-org/mason.nvim" },
-	{ src = "https://github.com/saghen/blink.cmp",              version = "1.*" },
+	{ src = "https://github.com/saghen/blink.cmp",                version = "1.*" },
 	{ src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "main" },
 })
 
