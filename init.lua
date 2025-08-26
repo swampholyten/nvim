18a7adc3e29392a55e5a1367c59b1930b1cc5cd3
Junkai Ji <98195504+swampholyten@users.noreply.github.com>
2025-08-26 14:28:55 +0200

    Update init.lua

--- init.lua
+++ init.lua
@@ -13,27 +13,26 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.termguicolors = true
 vim.opt.signcolumn = "yes"
-vim.opt.winborder = "single"
 
 vim.pack.add({
-	{ src = "https://github.com/vague2k/vague.nvim" },
+	{ src = "https://github.com/ellisonleao/gruvbox.nvim" },
+	{ src = "https://github.com/mason-org/mason.nvim" },
 	{ src = "https://github.com/neovim/nvim-lspconfig" },
+	{ src = "https://github.com/stevearc/oil.nvim" },
 	{ src = "https://github.com/ibhagwan/fzf-lua" },
 	{ src = "https://github.com/kdheepak/lazygit.nvim" },
-	{ src = "https://github.com/echasnovski/mini.surround" },
 	{ src = "https://github.com/christoomey/vim-tmux-navigator" },
-	{ src = "https://github.com/stevearc/oil.nvim" },
-	{ src = "https://github.com/mason-org/mason.nvim" },
 	{ src = "https://github.com/saghen/blink.cmp",              version = "1.*" },
 })
 
 require("mason").setup()
-require("mini.surround").setup()
-require("fzf-lua").setup()
+require("fzf-lua").setup({ "hide" })
+
 require("blink.cmp").setup({
 	keymap = { ["<CR>"] = { "accept", "fallback" } },
 	signature = { enabled = true },
 })
+
 require("oil").setup({
 	keymaps = {
 		["<C-h>"] = false,
@@ -41,35 +40,26 @@ require("oil").setup({
 	}
 })
 
-require("vague").setup({ transparent = true })
-vim.cmd("colorscheme vague")
-vim.cmd("hi statusline guibg=NONE")
+require("gruvbox").setup()
+vim.cmd("colorscheme gruvbox")
 
-vim.lsp.enable({ "rust_analyzer", "lua_ls", "vtsls", "tailwindcss", "html", "clangd", })
-
-local fzf = require("fzf-lua")
+vim.lsp.enable({ "rust_analyzer", "lua_ls", "vtsls", "clangd", })
 
 vim.keymap.set("n", "<leader>wo", ":wincmd c<CR>")
 
 vim.keymap.set({ "n", "v" }, "<leader>y", '"+y')
 vim.keymap.set({ "n", "v" }, "<leader>p", '"+p')
 
-vim.keymap.set("i", "{", "{}<Left>")
-vim.keymap.set("i", "(", "()<Left>")
-vim.keymap.set("i", "[", "[]<Left>")
-vim.keymap.set("i", "`", "``<Left>")
-vim.keymap.set("i", '"', '""<Left>')
-
-vim.keymap.set("n", "<leader>g", ":LazyGit<CR>")
 vim.keymap.set("n", "<leader>e", ":Oil<CR>")
+vim.keymap.set("n", "<leader>g", ":LazyGit<CR>")
 
+vim.keymap.set("n", "gd", vim.lsp.buf.definition)
 vim.keymap.set("n", "<leader>lf", vim.lsp.buf.format)
 vim.keymap.set("n", "<leader>k", vim.diagnostic.open_float)
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist)
+vim.keymap.set("n", "<leader>Q", vim.diagnostic.setqflist)
 
-vim.keymap.set("n", "<leader>q", fzf.diagnostics_document)
-vim.keymap.set("n", "<leader>Q", fzf.diagnostics_workspace)
-vim.keymap.set("n", "<leader>s", fzf.lsp_document_symbols)
-vim.keymap.set("n", "<leader>f", fzf.files)
-vim.keymap.set("n", "<leader><leader>", fzf.buffers)
+local fzf = require("fzf-lua")
+
+vim.keymap.set("n", "<leader>f", fzf.global)
 vim.keymap.set("n", "<leader>/", fzf.live_grep)
-vim.keymap.set("n", "<leader>r", fzf.oldfiles)
